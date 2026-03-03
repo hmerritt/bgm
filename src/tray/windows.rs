c7738fd0cfab0e5dc86021d8b6a48122fefb48fa
hmerritt <harry@mrrtt.me>
2026-03-03 22:24:09 +0000

    feat: add version into tray

--- src/tray/windows.rs
+++ src/tray/windows.rs
@@ -1,5 +1,6 @@
 use crate::errors::Result;
 use crate::tray::{format_running_duration, tray_stat_visibility, SessionStats, TrayEvent};
+use crate::version;
 use anyhow::{anyhow, bail};
 use std::mem::size_of;
 use std::path::PathBuf;
@@ -315,7 +316,9 @@ unsafe fn show_context_menu(hwnd: HWND, data: &WindowData) {
     let shown_value = data.session_stats.images_shown().to_string();
     let skipped_value = data.session_stats.manual_skips().to_string();
     let running_value = format_running_duration(data.session_stats.running_duration());
+    let app_version_value = version::get_version().full_version_number(false);
 
+    let app_version_label = wide_null(&app_version_value);
     let timer_label = wide_null(&format_stat_row("Timer", &timer_value));
     let remote_update_label = wide_null(&format_stat_row("Remote Update", &remote_update_value));
     let app_update_label = wide_null(&format_stat_row("App Update", &app_update_value));
@@ -352,6 +355,14 @@ unsafe fn show_context_menu(hwnd: HWND, data: &WindowData) {
     );
 
     let mut position: u32 = 0;
+    if !insert_disabled_menu_item(menu, position, app_version_label.as_ptr()) {
+        tracing::warn!("failed to add version tray menu item");
+    }
+    position += 1;
+    if !insert_separator_menu_item(menu, position) {
+        tracing::warn!("failed to add version separator tray menu item");
+    }
+    position += 1;
     if stat_visibility.timer {
         if !insert_disabled_menu_item(menu, position, timer_label.as_ptr()) {
             tracing::warn!("failed to add Timer tray menu item");
